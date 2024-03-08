7ea5ea5d7ca551b907b54a5f23d2a84edda20c65
JCubed71 <jenkjj5@farmingdale.edu>
2024-03-08 02:00:01 -0500

    First commit with 4 completed improvments
    03/08/2024
    Completed 4 coding improvements.
    1. The ability to set a primary Email from the list in New/Edit Contact
    2. Added an alert if an Email or Address was typed, but not added to the list when saving
    3. Updated contact grid to display primary email.
    4. Added datepicker and dropdown for title

---
+++ ContactManagerStarter/Models/ContactViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactManager.Data;
+
+namespace ContactManager.Models
+{
+    public class ContactViewModel
+    {
+        public List<Contact> Contacts { get; set; }
+        public List<EmailViewModel> EmailAddresses { get; set; }//Added by Jordan (may need to do Emails instead of EmailAddresses)
+        
+    }
+}
